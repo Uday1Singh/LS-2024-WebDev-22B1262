eb3c83a0480d9ef6fcdd3575a66fa9e7d559341d
Uday1Singh <147001691+Uday1Singh@users.noreply.github.com>
2024-07-09 07:10:58 +0530

    Add files via upload

---
+++ week-1/script.js
@@ -0,0 +1,35 @@
+
+function checkAnswer(answer) {
+    if (answer === 'correct') {
+        alert('Correct! A bowler can bowl a maximum of 10 overs in an ODI match.');
+    } else {
+        alert('Incorrect. Try again!');
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
